14680aff2de34a1a180408d9557d2ecf5649cf22
declspecl <gavind2559@gmail.com>
2024-01-21 21:12:57 -0500

    fixed structure error in previous commit

---
+++ src-tauri/src/model/mod.rs
@@ -0,0 +1,6 @@
+pub mod day;
+pub mod time;
+pub mod partition;
+
+pub use day::Day;
+pub use time::Time;
--- src-tauri/src/model/partition/activation_query.rs
+++ src-tauri/src/model/partition/activation_query.rs
@@ -1,3 +1,5 @@
+use crate::model::{Day, Time};
+
 #[derive(Debug)]
 pub enum ActivationQuery
 {
