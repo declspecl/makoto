d86152e11c41ab2b44c34eaeac0e28d4f4d5cce4
declspecl <gavind2559@gmail.com>
2024-01-27 23:26:45 -0500

    fixed styling and added shades to primary and secondary, other colors in future

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,11 +38,33 @@ const config: Config = {
                 foreground: "hsl(var(--card-foreground))"
             },
             primary: {
-                DEFAULT: "hsl(var(--primary))",
+                DEFAULT: "hsl(var(--primary-500))",
+                50: "hsl(var(--primary-50))",
+                100: "hsl(var(--primary-100))",
+                200: "hsl(var(--primary-200))",
+                300: "hsl(var(--primary-300))",
+                400: "hsl(var(--primary-400))",
+                500: "hsl(var(--primary-500))",
+                600: "hsl(var(--primary-600))",
+                700: "hsl(var(--primary-700))",
+                800: "hsl(var(--primary-800))",
+                900: "hsl(var(--primary-900))",
+                950: "hsl(var(--primary-950))",
                 foreground: "hsl(var(--primary-foreground))"
             },
             secondary: {
-                DEFAULT: "hsl(var(--secondary))",
+                DEFAULT: "hsl(var(--secondary-500))",
+                50: "hsl(var(--secondary-50))",
+                100: "hsl(var(--secondary-100))",
+                200: "hsl(var(--secondary-200))",
+                300: "hsl(var(--secondary-300))",
+                400: "hsl(var(--secondary-400))",
+                500: "hsl(var(--secondary-500))",
+                600: "hsl(var(--secondary-600))",
+                700: "hsl(var(--secondary-700))",
+                800: "hsl(var(--secondary-800))",
+                900: "hsl(var(--secondary-900))",
+                950: "hsl(var(--secondary-950))",
                 foreground: "hsl(var(--secondary-foreground))"
             },
             accent: {
