78aaeebb4374ef76bbca248ca057173e0a6514ee
dec-lied <gavindhondt@hotmail.com>
2024-02-05 12:13:28 -0500

    forgot to stage CalendarCell

--- src/components/CalendarCell.tsx
+++ src/components/CalendarCell.tsx
@@ -1,13 +1,18 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 interface CalendarCellProps extends React.HTMLAttributes<HTMLDivElement> {
-    dayNumber: number
+    dayNumber: number,
 }
 
 export function CalendarCell({ dayNumber, className }: CalendarCellProps) {
     return (
-        <div>
-            
+        <div className={cn(className)}>
+            <h3>{dayNumber}</h3>
+
+            <div className="flex flex-col">
+            {/* TODO: this is where most of the partition logic will go. will need to iterate through raw partitions and partition rules to see if the partition should be displayed here */}
+            </div>
         </div>
     );
 }
