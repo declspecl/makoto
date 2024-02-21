53167921ff8c9679be45db05aaa2067a5db2b122
dec-lied <gavindhondt@hotmail.com>
2024-02-21 10:59:27 -0500

    updated frontend commands file

--- src/backend/commands.ts
+++ src/backend/commands.ts
@@ -1,28 +1,10 @@
-import { MakotoData } from "./data";
 import { MakotoState } from "./state";
-import { MakotoConfig } from "./config";
 import { invoke } from "@tauri-apps/api";
 
-export function get_state(): Promise<MakotoState> {
-    return invoke<MakotoState>("get_state");
+export function try_serialize_state_to_disk(state: MakotoState): Promise<void> {
+    return invoke("try_serialize_state_to_disk", { state: state });
 }
 
-export function update_state_config(new_config: MakotoConfig): Promise<void> {
-    return invoke("update_state_config", { new_config });
-}
-
-export function update_state_data(new_data: MakotoData): Promise<void> {
-    return invoke("update_state_data", { new_data });
-}
-
-export function get_startup_error_log(): Promise<string[]> {
-    return invoke<string[]>("get_startup_error_log");
-}
-
-export function try_serialize_state_config_to_disk(): Promise<void> {
-    return invoke("try_serialize_state_config_to_disk");
-}
-
-export function try_serialize_state_data_to_disk(): Promise<void> {
-    return invoke("try_serialize_state_data_to_disk");
+export function try_deserialize_state_from_disk(): Promise<MakotoState> {
+    return invoke("try_deserialize_state_from_disk");
 }
